6e983b68e53690b73c95eb14685e10267413368c
vmcustodio <van.custodio.vc@gmail.com>
2022-12-23 12:00:21 -0300

    feat: calculator with teste TDD

---
+++ src/lib/calculator.spec.js
@@ -0,0 +1,23 @@
+const {sum} = require('./calculator')
+
+it('should sum 2 and 2 and the result must be 4', () => {
+  expect(sum(2,2)).toBe(4);
+});
+
+it('should sum 2 and 2 even if one of them is a string', () => {
+  expect(sum('2', 2)).toBe(4);
+});
+
+it('should throw an error if what is provided to the method cant be summed ', () => {
+  expect(() => {
+    sum('', '2')
+  }).toThrowError()
+
+  expect(() => {
+    sum([2, 2])
+  }).toThrowError()
+
+  expect(() => {
+    sum({})
+  }).toThrowError()
+});
