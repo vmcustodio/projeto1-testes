901142266bd175cd66bb67cc52dad55ec4f1d7c3
vmcustodio <van.custodio.vc@gmail.com>
2022-12-23 13:59:53 -0300

    feat: add pretiier

--- src/lib/queryString.spec.js
+++ src/lib/queryString.spec.js
@@ -1,13 +1,11 @@
-const { queryString } = require('./queryString')
+const { queryString } = require('./queryString');
 
 describe('Object to query string', () => {
   it('should create a valid query string when a object is provided', () => {
     const obj = {
       name: 'Fabio',
-      profession: 'developer'
-    }
-    expect(queryString(obj)).toBe(
-      'name=Fabio&profession=developer'
-    )
-  })
-})
+      profession: 'developer',
+    };
+    expect(queryString(obj)).toBe('name=Fabio&profession=developer');
+  });
+});
