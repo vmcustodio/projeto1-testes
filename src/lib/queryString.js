53f44ab1c347f68f399b3286f694dcfb0d2dbad4
vmcustodio <van.custodio.vc@gmail.com>
2022-12-26 22:28:01 -0300

    feat: query to object

--- src/lib/queryString.js
+++ src/lib/queryString.js
@@ -8,3 +8,11 @@ const keyValueToString = ([key, value]) => {
 
 module.exports.queryString = (obj) =>
   Object.entries(obj).map(keyValueToString).join('&');
+
+module.exports.parse = (string) => {
+  return Object.fromEntries(
+    string.split('&').map((item) => {
+      return item.split('=');
+    })
+  );
+};
