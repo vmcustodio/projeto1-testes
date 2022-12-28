2a926674b63a4e6892111177487fc146e4641c78
vmcustodio <van.custodio.vc@gmail.com>
2022-12-27 23:54:16 -0300

    feat: add tests cart add remove

--- src/lib/Cart.spec.js
+++ src/lib/Cart.spec.js
@@ -2,28 +2,41 @@ import Cart from './Cart';
 
 describe('Cart', () => {
   let cart;
+  let product = {
+    title: 'Adidas running shoes - men',
+    price: 35388, // 353.88 | R$ 383,88
+  };
 
   beforeEach(() => {
-    // antes de cada teste ser executado sempre terá um novo sendo feito do 0
     cart = new Cart();
   });
 
-  describe('getTotal()', () => {
-    it('should return 0 when getTotal() is executed in a newly created instance', () => {
-      expect(cart.getTotal()).toEqual(0);
-    });
+  it('should return 0 when getTotal() is executed in a newly created instance', () => {
+    expect(cart.getTotal()).toEqual(0);
   });
+
   it('should multiply quantity and price and receive the total amount', () => {
     const item = {
-      product: {
-        title: 'Adidas running shoes-men',
-        price: 35388,
-      },
+      product,
       quantity: 2,
     };
 
-    cart.addToCart(item);
+    cart.add(item);
 
     expect(cart.getTotal()).toEqual(70776);
+  });
+
+  it('should ensure no more than on product exists at a time', () => {
+    cart.add({
+      product,
+      quantity: 2,
+    });
+
+    cart.add({
+      product,
+      quantity: 1,
+    });
+
+    expect(cart.getTotal()).toEqual(35388);
   });
 });
